eb4b557a5dfb64282f51f159eac173a600501d5c
prottoy <prottoyroyaiub@gmail.com>
2021-07-25 17:24:18 +0600

    bookcontroller

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController:ControllerBase
+    {
+        
+    }
+}
